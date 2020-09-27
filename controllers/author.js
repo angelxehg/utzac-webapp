f99029028ce4364ef68651b4138aba9d4547b2da
Angel Hurtado <contact@angelxehg.com>
2020-09-27 15:25:02 -0500

    Author controller

--- controllers/author.js
+++ controllers/author.js
@@ -2,47 +2,93 @@
 
 const Author = require('../models/author');
 
-function saveAuthor(req, res) {
+function error(message, data) {
+  return {
+    error: true,
+    message: message,
+    content: data
+  };
+}
+
+function missing(parameter) {
+  return {
+    error: true,
+    message: `Missing '${parameter}' parameter`
+  };
+}
+
+function getAll(req, res) {
+  Author.find().then((authors) => {
+    return res.send(authors);
+  }).catch((err) => {
+    return res.status(400).send(error('Can\'t get authors', err));
+  })
+}
+
+function getOne(req, res) {
+  const id = req.params.author;
+  Author.findById(id).then((author) => {
+    if (!author) {
+      return res.status(400).send(error('Can\'t get author', err));
+    }
+    return res.send(author);
+  }).catch((err) => {
+    return res.status(400).send(error('Can\'t get author', err));
+  })
+}
+
+function create(req, res) {
   const params = req.body;
-
-  if (params.name === '' || params.name === null) {
-    return res.status(400).send({
-      error: true,
-      message: "Missing 'name' parameter"
-    });
+  if (params.name == '' || params.name == null) {
+    return res.status(400).send(missing('name'));
   }
-  if (params.country === '' || params.country === null) {
-    return res.status(400).send({
-      error: true,
-      message: "Missing 'country' parameter"
-    });
+  if (params.country == '' || params.country == null) {
+    return res.status(400).send(missing('country'));
   }
-
   var author = new Author();
   author.name = params.name;
   author.country = params.country;
+  author.save().then((author) => {
+    return res.status(201).send(author);
+  }).catch((err) => {
+    return res.status(400).send(error('Can\'t save author', err));
+  })
+}
 
-  author.save((err, data) => {
-    if (err) {
-      return res.status(400).send({
-        error: true,
-        message: "Error saving author",
-        content: err
-      });
-    } else if (data) {
-      return res.status(200).send({
-        message: "Author saved",
-        data: data
-      });
-    } else {
-      return res.status(500).send({
-        error: true,
-        message: "Error"
-      });
-    }
+function update(req, res) {
+  const id = req.params.author;
+  const params = req.body;
+  if (params.name == '' || params.name == null) {
+    return res.status(400).send(missing('name'));
+  }
+  if (params.country == '' || params.country == null) {
+    return res.status(400).send(missing('country'));
+  }
+  Author.findById(id).then((author) => {
+    author.name = params.name;
+    author.save().then((author) => {
+      return res.send(author);
+    }).catch((err) => {
+      return res.status(400).send(error('Can\'t update author', err));
+    });
+  }).catch((err) => {
+    return res.status(400).send(error('Can\'t get author', err));
+  });
+}
+
+function destroy(req, res) {
+  const id = req.params.author;
+  Author.findByIdAndDelete(id).then(() => {
+    return res.status(204).send();
+  }).catch((err) => {
+    return res.status(400).send(error('Can\'t delete author', err));
   });
 }
 
 module.exports = {
-  saveAuthor
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy
 };
--- routes/author.js
+++ routes/author.js
@@ -5,6 +5,10 @@ var AuthorController = require('../controllers/author');
 
 var api = express.Router();
 
-api.post('/author', AuthorController.saveAuthor);
+api.get('/authors', AuthorController.getAll);
+api.post('/authors', AuthorController.create);
+api.get('/authors/:author', AuthorController.getOne);
+api.put('/authors/:author', AuthorController.update);
+api.delete('/authors/:author', AuthorController.destroy);
 
 module.exports = api;
