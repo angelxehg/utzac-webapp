f8215b1089f5b764748ea66d4d7ba66c4571c07b
Angel Hurtado <contact@angelxehg.com>
2020-10-21 10:47:32 -0500

    apply error handling in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,37 +3,30 @@
 const jwt = require('jwt-simple');
 const moment = require('moment');
 const secret = process.env.SECRET || 'mi-clave-privada';
+const error = require('../helpers/error-handler');
 
 const logged = (req, res, next) => {
-  try {
-    if (!req.headers.authorization) {
-      throw 'No authorization provided';
-    }
-    const token = req.headers.authorization.replace(/[""]+/g, '');
-    const payload = jwt.decode(token, secret);
-    const current = moment().unix();
-    if (payload.exp <= current) {
-      throw 'Token expired';
-    }
-    req.user = payload;
-    next();
-  } catch (error) {
-    return res.status(403).send({ message: error });
+  if (!req.headers.authorization) {
+    throw error.unauthorized('No authorization provided');
   }
+  const token = req.headers.authorization.replace(/[""]+/g, '');
+  const payload = jwt.decode(token, secret);
+  const current = moment().unix();
+  if (payload.exp <= current) {
+    throw error.unauthorized('Token expired');
+  }
+  req.user = payload;
+  next();
 }
 
 const admin = (req, res, next) => {
-  try {
-    if (!req.user) {
-      throw 'Not logged as admin';
-    }
-    if (req.user.role !== 'ROLE_ADMIN') {
-      throw 'Not logged as admin';
-    }
-    next();
-  } catch (error) {
-    return res.status(403).send({ message: error });
+  if (!req.user) {
+    throw error.unauthorized('Not logged as admin');
   }
+  if (req.user.role !== 'ROLE_ADMIN') {
+    throw error.unauthorized('Not logged as admin');
+  }
+  next();
 }
 
 module.exports = {
