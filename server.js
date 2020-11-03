393fdfa0d220b3d649c21a6be17150735de9671e
Angel Hurtado <contact@angelxehg.com>
2020-11-03 14:28:02 -0600

    Move all to backend dir

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 const database = process.env.DATABASE_URL || 'mongodb://localhost:27017/utzac-webapp';
-const error = require('./helpers/error-handler');
+const error = require('./backend/helpers/error-handler');
 
-const api = require('./routes/api');
+const api = require('./backend/routes/api');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
