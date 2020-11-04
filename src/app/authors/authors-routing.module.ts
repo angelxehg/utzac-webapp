672549a828cb2e98dae8248488636e502678eca8
Angel Hurtado <contact@angelxehg.com>
2020-11-03 19:44:23 -0600

    Create AuthorDetails component

--- src/app/authors/authors-routing.module.ts
+++ src/app/authors/authors-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorDetailsComponent } from './author-details/author-details.component';
 import { AuthorsComponent } from './authors.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AuthorsComponent
+    component: AuthorsComponent,
+    children: [
+      {
+        path: ':id',
+        component: AuthorDetailsComponent
+      },
+      {
+        path: 'new',
+        component: AuthorDetailsComponent
+      }
+    ]
   },
 ];
 
--- src/app/authors/authors.module.ts
+++ src/app/authors/authors.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorsComponent } from './authors.component';
 import { AuthorsRoutingModule } from './authors-routing.module';
+import { AuthorDetailsComponent } from './author-details/author-details.component';
 
 @NgModule({
   declarations: [
-    AuthorsComponent
+    AuthorsComponent,
+    AuthorDetailsComponent
   ],
   imports: [
     CommonModule,
