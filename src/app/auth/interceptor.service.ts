0020b01c1933808a28455bf26cdbf0bc8a2f998a
Angel Hurtado <contact@angelxehg.com>
2020-11-04 14:31:23 -0600

    refactor interceptor

--- src/app/auth/interceptor.service.ts
+++ src/app/auth/interceptor.service.ts
@@ -1,7 +1,6 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, EMPTY } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,32 +8,20 @@ import { AuthService } from './auth.service';
 })
 export class InterceptorService implements HttpInterceptor {
 
-  skip = ['/auth/login', '/auth/register'];
+  skipUrls = ['/auth/login', '/auth/register'];
 
   constructor(private auth: AuthService) { }
 
-  skipRequest(request: HttpRequest<any>): boolean {
-    let skip = false;
-    this.skip.forEach(url => {
-      if (request.url.includes(url)) {
-        console.log('Skip request');
-        return skip = true;
-      }
-    });
-    return skip;
-  }
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let request = req;
-    if (this.skipRequest(request)) {
+    const skip = this.skipUrls.some(url => request.url.includes(url));
+    if (skip) {
       return next.handle(request);
     }
     const token = this.auth.token();
     if (!token) {
-      console.error('No token! canceling request!');
-      return EMPTY;
+      throw { error: { message: 'No ha iniciado sesión' } };
     }
-    console.log('Request intercepted');
     request = req.clone({
       setHeaders: { authorization: `${token}` }
     });
